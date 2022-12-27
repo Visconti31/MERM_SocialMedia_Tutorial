768d43ce43d9898515c12ae489ca4a5aa32e344c
João Pedro Visconti <visconti.joao@gmail.com>
2022-12-27 11:41:25 +0000

    Connect the server to the database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,29 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+// initialize the App
+const app = express()
+
+// General setup
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL =
+  'mongodb+srv://user01:test123456@technotesdb.7xvs5aq.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message))
+
+mongoose.set('strictQuery', true) // Suggested by mongoose
+
+// mongoose.set('useFindAndModify', false) // Is given some error
